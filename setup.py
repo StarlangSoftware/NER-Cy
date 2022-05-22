e684651750f8a92c008deb25d733a991b1528b3a
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-22 23:14:27 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 from Cython.Build import cythonize
 
 setup(
@@ -10,7 +10,7 @@ setup(
                            "NER/AutoProcessor/Sentence/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-NER-Cy',
-    version='1.0.3',
+    version='1.0.4',
     packages=['NER', 'NER.AutoProcessor', 'NER.AutoProcessor.Sentence', 'NER.AutoProcessor.ParseTree'],
     package_data={'NER.AutoProcessor.ParseTree': ['*.pxd', '*.pyx', '*.c', '*.py'],
                   'NER.AutoProcessor.Sentence': ['*.pxd', '*.pyx', '*.c', '*.py']},
@@ -19,7 +19,7 @@ setup(
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='NER library',
-    install_requires = ['NlpToolkit-AnnotatedSentence-Cy', 'NlpToolkit-AnnotatedTree-Cy'],
+    install_requires = ['NlpToolkit-AnnotatedTree-Cy'],
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
